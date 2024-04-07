0fb446adc23162e66be31ebfe63b1a1ccbd8c150
Choomper1230 <julianfelipe885@gmail.com>
2024-04-06 19:16:27 -0500

    V3.0

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,86 @@
+//Declaraciones de Variables
+
 let Total = 0;
 let aumento = "0";
 let anteriorOperador;
+let romper = 0;
 const colorUno = document.querySelector(".color1");
 const colorDos = document.querySelector(".color2");
 const colorTres = document.querySelector(".color3");
-const colorFondo = document.documentElement.style;
+const root = document.documentElement.style;
 const botonAct = document.querySelector("#actualizar")
+const menu = document.querySelector(".menu")
+const botonMenu = document.querySelector("#siguiente")
+const botonAtras = document.querySelector("#atras")
+const body = document.querySelector("#body")
+const difuminado = document.querySelector(".difuminado")
+const grados = document.querySelector("#grados")
+const opcionColor = document.querySelector('.boton-padre')
+const opcionGrados = document.querySelector('.grados')
 
-botonAct.addEventListener('click', () => {
-    colorFondo.setProperty("--colorUno", colorUno.value)
-    colorFondo.setProperty("--colorDos", colorDos.value)
-    colorFondo.setProperty("--colorTres", colorTres.value)
+//Botones de Menu
+
+botonMenu.addEventListener('click', () => {
+    root.setProperty("--moverAn", "moverUno 1s forwards");
+    root.setProperty("--moverAp", "desaparecer 1s forwards");
+    menu.style.display = "block"
+    difuminado.style.display = "block"
+    setTimeout(function() {
+        botonMenu.style.display = "none"
+        botonAtras.style.display = "block"
+        root.setProperty("--moverAp", "aparecer 1s forwards");
+    },1000)
+})
+botonAtras.addEventListener('click', () => {
+    root.setProperty("--moverAn", "moverDos 1s forwards");
+    root.setProperty("--moverAp", "desaparecer 1s forwards");
+    difuminado.style.display = "none"
+    setTimeout(function() {
+        botonAtras.style.display = "none"
+        menu.style.display = "none"
+        botonMenu.style.display = "block"
+        root.setProperty("--moverAp", "aparecer 1s forwards");
+    },1000)
 })
 
+//Grados
+
+grados.addEventListener('input', () => {
+    root.setProperty("--grados", grados.value + "deg")
+})
+
+//Color Picker
+
+colorUno.addEventListener('input', () =>{
+    root.setProperty("--colorUno", colorUno.value)
+})
+
+colorDos.addEventListener('input', () =>{
+    root.setProperty("--colorDos", colorDos.value)
+    
+    let valor = colorDos.value;
+
+        for (let i of valor) {
+            if (i < 6) {
+                root.setProperty("--colorBorde", "white")
+                break;
+            }
+            else{
+                romper++;
+                if (romper === 2) {
+                    root.setProperty("--colorBorde", "black")
+                    break;
+                }
+            }
+        }
+    romper = 0;
+})
+
+colorTres.addEventListener('input', () =>{
+    root.setProperty("--colorTres", colorTres.value)
+})
+
+//Funcionamiento de Calculadora
 
 const pantalla = document.querySelector('.pantalla');
 
